a372896732ca03d6ae774faacc7f285d7db86a75
Dino Grgić <dino.grgic1@gmail.com>
2023-04-08 18:58:46 +0200

    Added CUDA support.

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 import logging
-from preprocessing.VideoPreprocessing import VideoPreprocessing
-from preprocessing.ImagePreprocessing import ImagePreprocessing
+from time import sleep
+
+import cv2
 from python_json_config import ConfigBuilder
+
+from preprocessing.ImagePreprocessing import ImagePreprocessing
+from preprocessing.VideoPreprocessing import VideoPreprocessing
 
 CONFIG = ConfigBuilder().parse_config('config.json')
 
 if __name__ == '__main__':
     logging.basicConfig(level=CONFIG.logging.level, format=CONFIG.logging.format, datefmt=CONFIG.logging.date_format)
+
+    if cv2.cuda is not None:
+        cv2.cuda.printCudaDeviceInfo(cv2.cuda.getDevice())
 
     ip = ImagePreprocessing()
     ip.add_pipeline(ImagePreprocessing.bicubic_interpolation(0.6))
